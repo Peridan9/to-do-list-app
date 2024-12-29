60b07ebadcafd24277e8fd7533370932d247854f
Peridan9 <peridan@post.bgu.ac.il>
2024-12-29 14:25:15 +0200

    fixed Task model

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -4,7 +4,8 @@ const taskSchema = new mongoose.Schema({
   title: { type: String, required: true }, // Task title
   description: String,                     // Optional description
   status: { type: Boolean, default: false }, // Completed or not
-  due_date: Date                           // Optional due date
+  due_date: Date,                           // Optional due date
+  user_id: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
 });
 
 const Task = mongoose.model("Task", taskSchema);
