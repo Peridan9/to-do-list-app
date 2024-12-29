ebfba97165e457c2a03d87839db08b3acc829a21
Peridan9 <peridan@post.bgu.ac.il>
2024-12-29 15:38:44 +0200

    added authenticon routes

---
+++ backend/routes/auth.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/User');
+
+router.post("/register", async (req,res) => {
+    const {username, email, password, name, avatar} = req.body;
+
+    try{
+        // Check if the email or username is already taken
+        const existingUser = await User.findOne({ $or: [{email},{username}]});
+        if (existingUser){
+            return res.status(400).json({message: "Email or Username already exists"});
+        }
+        const newUser = new User({username, email, password, name, avatar});
+        await newUser.save();
+
+        res.status(201).json({ message: "User registered successfully", user: newUser });
+    } catch(err){
+        res.status(400).json({message: err.message});
+    }
+});
+
+router.post("/login", async (req,res) => {
+    const {email, password} = req.body;
+
+    try{
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(404).json({message:"User not found"});
+        }
+
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch){
+            return res.status(401).json({message:"Invalid credentials"});
+        }
+
+        res.status(200).json({message:"Login successful",user});
+    } catch(err){
+        res.status(500).json({message: err.message});
+    }
+});
+
