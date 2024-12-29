17e6f87aee5e90313a2295e8362b4078bba77d2a
Peridan9 <peridan@post.bgu.ac.il>
2024-12-29 19:29:00 +0200

    fixed all files needed for testing with jest

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const { swaggerUi, swaggerDocs } = require("./swagger/swagger-config");
-const tasksRoutes = require('./routes/tasks');
-require("./db")
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const authRoutes = require("./routes/auth");
+const tasksRoutes = require("./routes/tasks");
 
 const app = express();
 const PORT = 5000;
@@ -11,14 +11,15 @@ const PORT = 5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// Routes
+app.use("/users", authRoutes);
 app.use("/tasks", tasksRoutes);
 
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the To-Do List API");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+module.exports = app;
